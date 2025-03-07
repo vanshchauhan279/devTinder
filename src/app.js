26c270e0c114fc2dc395c1d0fe08a79652063922
vansh <chauhanvansh279@gmail.com>
2025-03-08 02:45:44 +0530

    Exploring Get Post Put Delete

--- src/app.js
+++ src/app.js
@@ -2,14 +2,29 @@ const express = require("express");
 const app = express();
 const port = 7777
 
+
+// app.use('/',(req,res)=>{
+//     res.send("message from the server")
+// })
+
 app.use('/hello',(req,res)=>{
     res.send("Hello Hello Hello")
 })
-app.use('/text',(req,res)=>{
-     res.send("This is text message")
+
+app.get('/user',(req,res)=>{
+    res.send({firstName: "Vansh", lastName: "Chauhan"})
 })
-app.use('/',(req,res)=>{
-    res.send("message from the server")
+
+app.post('/user',(req,res)=>{
+    res.send("Data Successful saved to database")
+})
+
+app.put('/user',(req,res)=>{
+    res.send("Data is updated")
+})
+
+app.delete('/user',(req,res)=>{
+     res.send("Record is deleted from Database")
 })
 
 app.listen(port,()=>{
