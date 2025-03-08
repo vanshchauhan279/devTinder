67b12659389112f81ca47258f9296b63a4606dc8
vansh <chauhanvansh279@gmail.com>
2025-03-08 11:07:47 +0530

    multiplt routing handles

--- src/app.js
+++ src/app.js
@@ -2,30 +2,22 @@ const express = require("express");
 const app = express();
 const port = 7777
 
+app.get('/user',(req,res,next)=>{
+     console.log("1 Router")
+     next()
+    //  res.send("This is 1 Response")
+},
+(req,res,next)=>{
+   console.log("2 Route")
+   next()
+//    res.send("This is a second Response")
+ 
 
-// app.use('/',(req,res)=>{
-//     res.send("message from the server")
-// })
-
-app.use('/hello',(req,res)=>{
-    res.send("Hello Hello Hello")
-})
-
-app.get('/user',(req,res)=>{
-    res.send({firstName: "Vansh", lastName: "Chauhan"})
-})
-
-app.post('/user',(req,res)=>{
-    res.send("Data Successful saved to database")
-})
-
-app.put('/user',(req,res)=>{
-    res.send("Data is updated")
-})
-
-app.delete('/user',(req,res)=>{
-     res.send("Record is deleted from Database")
-})
+},(req,res,next)=>{
+    console.log("3 Route")
+    res.send("This is a 3 Response")
+ 
+ })
 
 app.listen(port,()=>{
     console.log(`app listening on port ${port}`)
