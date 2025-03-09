d422dd56d6f7544f7b83d8d362e5415fd0ea8853
vansh <chauhanvansh279@gmail.com>
2025-03-10 00:49:04 +0530

    handle error

--- src/app.js
+++ src/app.js
@@ -1,22 +1,30 @@
 const express = require("express");
-const {adminAuth,userAuth}=require('./middleWare/Auth')
-
 const app = express();
 const port = 7777
 
-app.use('/admin',adminAuth)
-
-app.get('/user',userAuth,function(req,res){
-     res.send("user data send")
+app.use('/',function(req,res){
+    try{
+        console.log("try run when we use USE method")
+        throw new error()
+        res.send("data send")
+    }
+    catch(err){
+        res.status(400).send("Something went wrong")
+    } 
 })
 
-app.get('/admin/getAllData',(req,res)=>{
-        res.send("All Data Sent");    
+
+app.use('/',(err,req,res,next)=>{
+    if(err){
+        res.status(400).send("error occured here")
+    }
 })
 
-app.get('/admin/deleteUser',(req,res)=>{
-        res.send("Delete User Account");  
+app.get('/user',function(req,res){
+    res.send("user content")
 })
+
+
 
 app.listen(port,()=>{
     console.log(`app listening on port ${port}`)
